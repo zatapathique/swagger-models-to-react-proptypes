d416686bc13542f196aaf24bbe35b0d8d6eea448
Andrzej Dybionka <andrzej@arabel.la>
2018-03-01 00:09:01 +0100

    Added option to camelize prop types keys

--- parsers/1.2.js
+++ parsers/1.2.js
@@ -14,7 +14,7 @@ var unknownPropType = function(props, propName, componentName) {
     return new Error('The propType for \'' + propName + '\' could not be determined from Swagger model definition. Perhaps try \'React.PropTypes.any\'?');
 };
 
-var getPropType = function (definition) {
+var getPropType = function (definition, options) {
     if (definition.enum) {
         return 'React.PropTypes.oneOf(' + JSON.stringify(definition.enum, null, 4) + ')';
     }
@@ -27,7 +27,7 @@ var getPropType = function (definition) {
     if (!definition.type) {
         return 'React.PropTypes.shape({\n'
             + indent(_.map(definition.properties, function (property, name) {
-                var keyPropType = convertDefinitionObjectToPropTypes(property, name);
+                var keyPropType = convertDefinitionObjectToPropTypes(property, name, options);
                 if (_.contains(definition.required || [], name)) {
                     keyPropType += '.isRequired';
                 }
@@ -38,7 +38,7 @@ var getPropType = function (definition) {
 
     switch (definition.type) {
     case 'array':
-        return 'React.PropTypes.arrayOf(' + getPropType(definition.items) + ')';
+        return 'React.PropTypes.arrayOf(' + getPropType(definition.items, options) + ')';
     case 'string':
         return 'React.PropTypes.string';
     case 'integer':
@@ -50,18 +50,22 @@ var getPropType = function (definition) {
     }
 };
 
-var convertDefinitionObjectToPropTypes = function (definition, name) {
-    return name + ': ' + getPropType(definition);
+var convertDefinitionObjectToPropTypes = function (definition, name, options) {
+    if (options.camelizeKeys) {
+        name = _.camelCase(name);
+    }
+
+    return name + ': ' + getPropType(definition, options);
 };
 
-module.exports = function (swagger) {
+module.exports = function (swagger, options) {
     var header = 'Generated PropTypes for ' + swagger.url;
     console.log('\n/**\n\n' + header + '\n' + new Array(header.length + 1).join('-') + '\n\n**/\n\n');
 
     console.log('var PropTypes = {\n');
 
     var propTypes = _.map(swagger.models, function (model, name) {
-        return convertDefinitionObjectToPropTypes(model.definition, name);
+        return convertDefinitionObjectToPropTypes(model.definition, name, options);
     });
 
     console.log(indent(propTypes.join(',\n\n')));
